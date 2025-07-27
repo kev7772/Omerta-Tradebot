3be520412676b2018d997334de3b1a60f03d3690
kev7772 <k.clower@gmx.de>
2025-07-27 09:46:51 +0200

    Update simulator.py

--- bot/simulator.py
+++ bot/simulator.py
@@ -147,7 +147,7 @@ def evaluate_decision(decision, percent_change):
         return "Neutral / kein klarer Vorteil"
 
 def save_simulation_log(entries, batch=False):
-    filepath = "learning_log.json"
+    filepath = "simulation_log.json""
 
     try:
         try:
