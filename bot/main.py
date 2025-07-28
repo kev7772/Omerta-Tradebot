c9f7fd30b665c85fb305e684233b52b197a4289a
kev7772 <k.clower@gmx.de>
2025-07-28 21:14:08 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -320,3 +320,7 @@ if __name__ == '__main__':
     threading.Thread(target=run_scheduler).start()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+if not os.path.exists("ghost_log.json"):
+    with open("ghost_log.json", "w") as f:
+        json.dump([], f)
